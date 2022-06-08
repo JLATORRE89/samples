04cf7377e122559e50348c3eb18f5f315b909273
Jason <jlatorre1989@hotmail.com>
2022-06-07 19:03:54 -0500

    Update yum.sh

--- patchManager/scripts/yum.sh
+++ patchManager/scripts/yum.sh
@@ -2,12 +2,14 @@
 # This script requires the following:
 # 1. That the user be on the system you are SSHing to.
 # 2. That there is an SSH key installed and usable on the remote system
-# 3. That the user attempting to access YUM is defined in the SUDOERs file.
+# 3. That the SSH key is accessible by the apache user
+# 4. That the user attempting to access YUM is defined in the SUDOERs file.
 # Example Definition: builder ALL=(ALL) NOPASSWD: /bin/rpm, /usr/bin/up2date, /usr/bin/yum
 # If more rights are requested, enable the sys group and add the respective user to the group.
 #whoami
 user=builder
-ssh $user@localhost 'yum clean all'
-ssh $user@localhost 'bash -s' < job.sh
+key=/var/www/id_rsa
+ssh -i $key -o StrictHostKeyChecking=no $user@localhost 'yum clean all'
+ssh -i $key $user@127.0.0.1 'bash -s' < job.sh
 #sudo yum clean all
 #sudo yum update -y
